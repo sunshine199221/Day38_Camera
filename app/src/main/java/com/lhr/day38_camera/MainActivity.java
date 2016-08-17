c3b2ccc684c70ff5fd2065aab1fec071293d3c52
Administrator <632780458@qq.com>
2016-08-17 16:41:18 +0800

    新建了分支

--- app/src/main/java/com/lhr/day38_camera/MainActivity.java
+++ app/src/main/java/com/lhr/day38_camera/MainActivity.java
@@ -23,9 +23,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         bt1= (Button) findViewById(R.id.bt1);
         iv1= (ImageView) findViewById(R.id.iv1);
-        /**
-         * 按钮的点击事件，打开照相机，并把拍摄的照片保存到sd卡中
-         */
         bt1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -33,14 +30,6 @@ public class MainActivity extends AppCompatActivity {
                 File file=new File( Environment.getExternalStorageDirectory(),"aaa.jpg");
                 in.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(file));
                 startActivityForResult(in,0);
-//                Intent in=new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-//                File file=new File( Environment.getExternalStorageDirectory(),"aaa.jpg");
-//                in.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(file));
-//                startActivityForResult(in,0);
-//                Intent in=new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-//                File file=new File( Environment.getExternalStorageDirectory(),"aaa.jpg");
-//                in.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(file));
-//                startActivityForResult(in,0);
             }
         });
     }
