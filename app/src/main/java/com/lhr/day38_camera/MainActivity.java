6ff168d31aed3915d73ed2160ead9643cbf1d9c2
Administrator <632780458@qq.com>
2016-08-17 17:37:24 +0800

    zengjiawuyongde

--- app/src/main/java/com/lhr/day38_camera/MainActivity.java
+++ app/src/main/java/com/lhr/day38_camera/MainActivity.java
@@ -33,6 +33,8 @@ public class MainActivity extends AppCompatActivity {
                 File file=new File( Environment.getExternalStorageDirectory(),"aaa.jpg");
                 in.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(file));
                 startActivityForResult(in,0);
+
+
 //                Intent in=new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
 //                File file=new File( Environment.getExternalStorageDirectory(),"aaa.jpg");
 //                in.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(file));
